f69b47e3f7ee566060de519495c274b447340153
Florian Finkernagel <TyberiusPrime@users.noreply.github.com>
2019-04-18 12:35:47 +0200

    removed unused max quant exports

--- src/mbf_comparisons/__init__.py
+++ src/mbf_comparisons/__init__.py
@@ -11,39 +11,8 @@ finally:
     del get_distribution, DistributionNotFound
 
 
-from .maxquant_parser import (  # noqa:F401
-    TableBasedDataset,
-    MaxQuantDataset,
-    Normalization_Raw,
-    Normalization_MedianCenter,
-    Normalization_MedianZero,
-    Normalization_Quantile,
-    Normalization_InverseNormalTransformation,
-    Zero_Ignore,
-    Zero_NaN,
-    Zero_Filter,
-    Zero_NaNIfOtherMeasured,
-    Contrast_TTest,
-    Contrast_TTestPaired,
-    Contrast_Limma,
-    Contrast_LimmaPaired,
-    Load_AllFeatureTable,
-)
-
 from .annotator import Comparison
 from .comparisons import Log2FC, TTest, TTestPaired, EdgeRUnpaired, DESeq2Unpaired
 
 
-def do_export(name):
-    if "_" in name:
-        start = name
-        start = start[: start.find("_")]
-        return start in ("Load", "Contrast", "Zero", "Normalization")
-    return False
-
-
-__all__ = (
-    ["TableBasedDataset", "MaxQuantDataset"]
-    + [key for (key, value) in globals().items() if do_export(key)]
-    + [Comparison, Log2FC, TTest, TTestPaired, EdgeRUnpaired, DESeq2Unpaired]
-)
+__all__ = [Comparison, Log2FC, TTest, TTestPaired, EdgeRUnpaired, DESeq2Unpaired]
