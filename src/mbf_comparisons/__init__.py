a1ef237c41c8a5cd1cfdbac6b0d2f6147a396fba
Florian Finkernagel <TyberiusPrime@users.noreply.github.com>
2019-05-10 09:28:33 +0000

    setup.cfg versioning

--- src/mbf_comparisons/__init__.py
+++ src/mbf_comparisons/__init__.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
-
+__version__ = "0.1"
 
 from .comparisons import Comparisons
 from .methods import Log2FC, TTest, TTestPaired, EdgeRUnpaired, DESeq2Unpaired
